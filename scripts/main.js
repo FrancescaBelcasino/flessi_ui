1c276cf82f721a1f4d7216869f1cdc46d95400e5
FrancescaBelcasino <franbelcasino@gmail.com>
2024-07-04 23:34:06 -0300

    [ADD] fetch_and_error_handling

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,11 @@
+const openModal = (jobId) => {
+    document.getElementById(jobId).style.display = 'flex';
+}
+
+const closeModal = (jobId) => {
+    document.getElementById(jobId).style.display = 'none';
+}
+
 const handleHomeSearch = () => {
     let category = document.getElementById("category").value;
     let location = document.getElementById("location").value;
@@ -5,10 +13,73 @@ const handleHomeSearch = () => {
     window.location.href = `./jobs.html?category=${category}&location=${location}`
 }
 
-const openModal = (jobId) => {
-    document.getElementById(jobId).style.display = 'flex';
+
+const handleLogin = (e) => {
+    e.preventDefault()
+
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    
+    fetch("URL", {
+        method: "POST",
+        body: {
+            "email": `${email}`,
+            "password": `${password}`
+        }
+    })
+    .then(r => {
+        if (r.status == 200) {
+            localStorage.setItem("user", JSON.stringify({
+                email: email,
+                password: password
+            }))
+        } else {
+            Swal.fire({
+                icon: "error",
+                iconColor: "#B22222",
+                title: "Oops...",
+                text: "Usuario o contraseña incorrectos!",
+                confirmButtonColor: "#57003e",
+                backdrop: false
+              });
+        }
+    })
 }
 
-const closeModal = (jobId) => {
-    document.getElementById(jobId).style.display = 'none';
+const handleRegister = (e) => {
+    e.preventDefault()
+
+    let username = document.getElementById("username").value
+    let name = document.getElementById("name").value
+    let surname = document.getElementById("surname").value
+    let email = document.getElementById("email").value
+    let password = document.getElementById("password").value
+    
+    fetch("URL", {
+        method: "POST",
+        body: {
+            "username": `${username}`,
+            "name": `${name}`,
+            "surname": `${surname}`,
+            "email": `${email}`,
+            "password": `${password}`
+        }
+    })
+    .then(r => {
+        if (r.status == 200) {
+            localStorage.setItem("user", JSON.stringify({
+                email: email,
+                password: password
+            }))
+        } else {
+            Swal.fire({
+                icon: "error",
+                iconColor: "#B22222",
+                title: "Oops...",
+                text: "Se produjo un error al registrar, por favor intente mas tarde...",
+                confirmButtonColor: "#57003e",
+                backdrop: false
+              });
+        }
+    })
 }
